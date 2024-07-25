415e2263a97dffdb15b81ce4db7c661b29301c09
Raymond Kim <rkim@tenstorrent.com>
2024-07-25 23:55:38 +0000

    #10750: Check pip version

--- scripts/check_stuff.sh
+++ scripts/check_stuff.sh
@@ -8,6 +8,6 @@ echo "[Info] Showing site-packages"
 ls -hal python_env/lib/python3.8/site-packages
 find python_env/lib/python3.8/site-packages -name *.egg* | xargs -n 1 -I {} bash -c 'echo {} && cat {}'
 which pip
-which pip3
 which python3
+python3 -m pip --version
 python3 -c 'import scripts; import ttnn; import loguru; print(loguru.__file__); print(scripts.__file__); print(ttnn.__file__); print(ttnn._ttnn.__file__);'
