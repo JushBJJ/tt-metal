519d75e9ef111d2ca498d502a0dde214c2995ec3
Raymond Kim <rkim@tenstorrent.com>
2024-07-25 23:52:37 +0000

    #10750: Check where the pips are

--- scripts/check_stuff.sh
+++ scripts/check_stuff.sh
@@ -7,4 +7,7 @@ ls -hal ttnn
 echo "[Info] Showing site-packages"
 ls -hal python_env/lib/python3.8/site-packages
 find python_env/lib/python3.8/site-packages -name *.egg* | xargs -n 1 -I {} bash -c 'echo {} && cat {}'
+which pip
+which pip3
+which python3
 python3 -c 'import scripts; import ttnn; import loguru; print(loguru.__file__); print(scripts.__file__); print(ttnn.__file__); print(ttnn._ttnn.__file__);'
