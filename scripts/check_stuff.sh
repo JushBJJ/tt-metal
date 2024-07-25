f01619ab7e50befdd893035e7bc70004d4080e20
Raymond Kim <rkim@tenstorrent.com>
2024-07-25 23:47:19 +0000

    #10750: Check loguru location first

--- scripts/check_stuff.sh
+++ scripts/check_stuff.sh
@@ -7,4 +7,4 @@ ls -hal ttnn
 echo "[Info] Showing site-packages"
 ls -hal python_env/lib/python3.8/site-packages
 find python_env/lib/python3.8/site-packages -name *.egg* | xargs -n 1 -I {} bash -c 'echo {} && cat {}'
-python3 -c 'import scripts; import ttnn; import loguru; print(scripts.__file__); print(ttnn.__file__); print(ttnn._ttnn.__file__); print(loguru.__file__);'
+python3 -c 'import scripts; import ttnn; import loguru; print(loguru.__file__); print(scripts.__file__); print(ttnn.__file__); print(ttnn._ttnn.__file__);'
