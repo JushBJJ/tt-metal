439210a1ea88ddaf2f92193e88ed66ab7b682db7
Raymond Kim <rkim@tenstorrent.com>
2024-07-25 23:00:47 +0000

    #10750: Check if we even have egg links at all

--- scripts/check_stuff.sh
+++ scripts/check_stuff.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+find python_env/lib/python3.8/site-packages -name *.egg* | xargs -n 1 -I {} bash -c 'echo {} && cat {}'
 python3 -c 'import scripts; import ttnn; import loguru; print(scripts.__file__); print(ttnn.__file__); print(ttnn._ttnn.__file__); print(loguru.__file__);'
