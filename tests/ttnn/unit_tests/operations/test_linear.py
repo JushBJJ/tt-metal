6e9e7d0dad5cf274db6350df8556790397a455a9
Borys Bradel <bbradel@tenstorrent.com>
2024-07-26 13:57:58 +0000

    #0: test_linear batch=88

--- tests/ttnn/unit_tests/operations/test_linear.py
+++ tests/ttnn/unit_tests/operations/test_linear.py
@@ -134,7 +134,7 @@ def test_linear_with_core_grid(
     assert_with_pcc(torch_output_tensor, output_tensor, 0.999)
 
 
-@pytest.mark.parametrize("batch_size", [1, 8])
+@pytest.mark.parametrize("batch_size", [1, 88])
 @pytest.mark.parametrize("m_size", [32, 64])
 @pytest.mark.parametrize("k_size", [1024, 2048])
 @pytest.mark.parametrize("n_size", [1024, 2048])
